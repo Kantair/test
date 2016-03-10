79b8619d48d1da52e9f70096dab8acffe03ac655
Kantair <tkachevanse@gmail.com>
2016-03-10 15:10:46 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,23 +2,17 @@
 # ini file for work
 
 # При входе в терминал надо запустить Ngnix и проверить:
-#sudo /etc/init.d/nginx start
+sudo /etc/init.d/nginx start
 # ps -o pid,euser,egroup,comm,args -C nginx
 
-#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-#sudo /etc/init.d/nginx restart
-
-#sudo ln -sf /home/box/web/etc/gunicorn_conf.py   /etc/gunicorn.d/test
-#sudo ln -sf /home/box/web/etc/gunicorn_conf_ask.py /etc/gunicorn.d/ask
-#sudo /etc/init.d/gunicorn restart
-
-#cd /home/box/web/
-#sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
-
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/test
+
+sudo ln -sf /home/box/web/etc/gunicorn_conf.py   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn_conf_ask.py /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
-cd /home/box/web/etc/
-sudo gunicorn -b 0.0.0.0:8080 hello:app
+
+cd /home/box/web/
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
+
+
