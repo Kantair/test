c825eb774b8683976992d0979a77fb0a1769e6fd
Kantair <tkachevanse@gmail.com>
2016-03-10 15:12:21 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,6 @@
-def wsgi_app(env, start_response):
-
-
-  status = '200 OK'
-  header = [('Content-Type', 'text/plain')]
-
-  body = []
-  for key in env['QUERY_STRING'].split('&'):
-    data = key + "\n"
-    body.append(data)
-
-
-  start_response(status, header)
-  return body
+def app(environ, start_response):
+    data = ''
+    for line in environ["QUERY_STRING"].split("&"):
+        data = data+line+"\n"
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [data]
